140cf6daf9e9636e938cd616a52b17566b99f877
Susanna Ånäs <susanna.anas@gmail.com>
2024-07-27 15:55:02 +0300

    small chnages
    Removed Display value, moved code to templates.js, added variables

--- templates.js
+++ templates.js
@@ -2,17 +2,34 @@
 
 function getTemplate(templateName) {
     const templates = {
-        displayValueHeaderBottom: `
-            <div>Display value specific options</div>
-        `,
         viewWebPageHeaderBottom: `
             <div>View web page specific options</div>
         `,
         viewWikimediaHeaderBottom: `
-            <div>View Wikimedia site specific options</div>
+            <div class="row">
+                <div class="bold-text">Project</div>
+                <select id="projectSelect">
+                    <option>Wikidocumentaries</option>
+                    <option>Wikipedia</option>
+                    <option>Wikimedia Commons</option>
+                    <option>Reasonator</option>
+                </select>
+                <select id="languageSelect">
+                    <option value="en" selected>English</option>
+                    <option value="fi">Finnish</option>
+                    <option value="sv">Swedish</option>
+                    <!-- Add more languages as needed -->
+                </select>
+            </div>
+            <div class="row">
+                <div id="wikidocumentariesLink"></div>
+                <div id="reasonatorLink"></div>
+            </div>
         `,
         compareCoordinatesHeaderBottom: `
-            <div>Compare coordinates specific options</div>
+            <div class="row">
+                <div class="bold-text">Dataset value</div> <a id="projectUrl" href="#" target="_blank">Simple text</a>
+            </div>
         `,
         compareDatesHeaderBottom: `
             <div>Compare dates specific options</div>
@@ -33,7 +50,6 @@ function getTemplate(templateName) {
 
 function generateComponentBody(viewType) {
     const bodies = {
-        displayValue: '<div>Display value component content</div>',
         viewWebPage: '<iframe src="https://example.com" style="width: 100%; height: 100%; border: none;"></iframe>',
         viewWikimedia: '<iframe src="https://www.wikimedia.org" style="width: 100%; height: 100%; border: none;"></iframe>',
         compareCoordinates: '<div id="map" style="width: 100%; height: 100%;"></div>',
