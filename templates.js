b9556482b1343e1f8a09b5fd2a6a2a53a4afb74d
Susanna Ånäs <susanna.anas@gmail.com>
2024-08-10 15:19:17 +0200

    more instructions, options

--- templates.js
+++ templates.js
@@ -101,12 +101,15 @@ function generateComponentBody(viewType) {
         `,
         reconciliationSettings: `
     <div class="componentContent">
+        <div>At the moment the settings cannot be changed. The idea is that the user could tweak the search settings for individual searches to limit or expand the search of candidates.</div>
         <h2>Data source</h2>
         <div class="propgrid grid-2">
             <div class="bold-text">Type:</div>
             <div class="rowcontent">
                 <select id="serviceSelect">
-                    <option>Google Spreadsheet</option>
+                    <option>Google Sheets</option>
+                    <option>OpenRefine</option>
+                    <option>Import data</option>
                 </select>
             </div>
             <div class="bold-text">Spreadsheet ID</div>
@@ -136,13 +139,13 @@ function generateComponentBody(viewType) {
             <h3 class="rowcontent bold-text grid-span-2">SPARQL</h3>
 
             <div></div>
-            <div class="rowcontent">Wikibase<br />First only Wikidata will be searched.</div>
+            <div class="rowcontent"><div class="bold-text">Wikibase</div>First only Wikidata will be searched.</div>
             <select id="serviceSelect">
                 <option value="wikidata" selected>Wikidata</option>
             </select>
 
             <div></div>
-            <div class="rowcontent">Endpoint<br />Wikidata SPARQL endpoint uses mwapi to make a
+            <div class="rowcontent"><div class="bold-text">Endpoint</div>Wikidata SPARQL endpoint uses mwapi to make a
                 text search in Wikidata, while QLever makes a link search in Wikipedia articles.</div>
             <select id="serviceSelect">
                 <option value="wikidata" selected>Wikidata Query Service</option>
@@ -150,7 +153,7 @@ function generateComponentBody(viewType) {
             </select>
 
             <div></div>
-            <div class="rowcontent">Language</div>
+            <div class="rowcontent"><div class="bold-text">Language</div></div>
             <select id="searchLanguageSelect">
                 <option value="en">English</option>
                 <option value="fi">Finnish</option>
@@ -158,19 +161,30 @@ function generateComponentBody(viewType) {
             </select>
 
             <div class="rowcontent grid-span-2-2 bold-text">Search with properties</div>
-            <div class="grid-3 grid-span-2-2 propgrid">
+            <div class="grid-4 grid-span-2-2 propgrid">
+
                 <div class="check">On/off</div>
-                <div class="rowcontent">Property</div>
+                <div class="rowcontent">Wikibase property</div>
                 <div class="rowcontent">Dataset value</div>
+                <div class="rowcontent">Delete</div>
+
                 <div class="check"><input type="checkbox" id="propcheck-1"></div>
                 <div class="rowcontent"><input type="text" placeholder="eg. 'P31'"></div>
                 <select id="colSelect">
                     <option>Select column</option>
                 </select>
+                <div class="rowcontent">Delete</div>
+
                 <div class="grid-span-3">
                     <button id="typeButton">Add property</button>
                 </div>
             </div>
+
+            <div class="check"><input type="radio" id="txtradio-2"></div>
+            <h3 class="rowcontent grid-span-2">Reconciliation API</h3>
+            <div class="grid-span-2-2">Make an internet search limited to Wikipedias and add the QIDs of the results as
+                reconciliation candidates.</div>
+            <div class="grid-span-2-2">Example search: homme-panthères site:*.wikipedia.org</div>
 
             <div class="check"><input type="radio" id="txtradio-2"></div>
             <h3 class="rowcontent grid-span-2">Search Wikipedias with Internet search engines</h3>
